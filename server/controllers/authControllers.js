bf216bbad25fa61c5c56c9605adbefbb9a9e66a2
ABDUL MOEEZ ASAD <aasad.bscs22seecs@seecs.edu.pk>
2024-05-20 00:25:01 +0500

    logout

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -226,8 +226,8 @@ const getProfile= async (req,res)=>{
 }
 const logOut =(req,res) =>{
     
-    res.clearCookie('token',{path:'/'});
-    res.clearCookie('connect.sid',{path:'/'})
+    res.clearCookie('token');
+    res.clearCookie('connect.sid')
     return res.json({Status:"Success"})
 }
 const PasswordReset = async (req, res) => {
