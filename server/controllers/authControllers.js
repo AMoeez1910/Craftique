582a5736c5fe7dfa82d906d048c1a6b469181179
ABDUL MOEEZ ASAD <aasad.bscs22seecs@seecs.edu.pk>
2024-05-20 01:07:27 +0500

    logout

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -225,12 +225,12 @@ const getProfile= async (req,res)=>{
   }
 }
 const logOut =(req,res) =>{
-    res.clearCookie('token',{
+    res.cookie('token', '', {
         path: '/',
         httpOnly: true,
         secure: true,  // Ensure this is true for HTTPS
         sameSite: 'None',  // Required for cross-site cookie sharing
-        maxAge: 24 * 60 * 60 * 1000  // 1 day
+        maxAge: 0  // Set maxAge to 0 to expire the cookie immediately
     });
     res.clearCookie('connect.sid');
     return res.json({Status:"Success"})
